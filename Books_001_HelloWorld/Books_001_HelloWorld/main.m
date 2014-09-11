77d8d1fa27388d89509708a480df423c88688b3f
oxcaesar <oxcaesar@gmail.com>
2014-09-11 13:43:34 +0800

    add operate the two number

--- Books_001_HelloWorld/Books_001_HelloWorld/main.m
+++ Books_001_HelloWorld/Books_001_HelloWorld/main.m
@@ -9,9 +9,23 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
+    @autoreleasepool { // system can manage the memory efficiently,auto reference counting
         // show the message on the console
         NSLog(@"Hello, World!");
+        // two number operate
+        int add_result, sub_result, multi_result, div_result;
+        int operate1 = 5;
+        int operate2 = 5;
+        add_result = operate1 + operate2;
+        sub_result = operate1 - operate2;
+        multi_result = operate1 * operate2;
+        div_result = operate1 / operate2;
+        
+        NSLog(@"%i + %i = %i", operate1, operate2, add_result);
+        NSLog(@"%i - %i = %i", operate1, operate2, sub_result);
+        NSLog(@"%i * %i = %i", operate1, operate2, multi_result);
+        NSLog(@"%i / %i = %i", operate1, operate2, div_result);
+        
     }
     return 0;
 }
