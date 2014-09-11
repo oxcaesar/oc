0d77f3479d21193ccda3f129025eff60ab0e631c
oxcaesar <oxcaesar@gmail.com>
2014-09-11 13:03:42 +0800

    HelloWorld from Books that show the basic program

---
+++ Books_001_HelloWorld/Books_001_HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Books_001_HelloWorld
+//
+//  Created by oxcaesar on 14-9-11.
+//  Copyright (c) 2014年 oxcaesar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // show the message on the console
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
